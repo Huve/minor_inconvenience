c33009a04930f558e8f8b62e637d3fe7880b2e47
Chris Pentoney <huvetahw@gmail.com>
2023-10-29 20:39:50 -0700

    Player info

---
+++ GameInfoNode.gd
@@ -0,0 +1,22 @@
+extends Node
+
+var player_level = 0
+var player_strength = 1
+var player_speed = 1
+var player_health = 1
+
+var level_count = 1
+var default_total_score = 0
+var total_score = 0
+var current_level_score = 0
+var global_level_time = 10
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
